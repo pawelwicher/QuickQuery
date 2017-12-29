9fbac0c11f0cd4cf0d7874277e5164461318e709
pawelwicher <pawel.wicher@op.pl>
2017-12-29 02:03:24 +0100

    QuickQuery code

--- QuickQuery.TestApp/Program.cs
+++ QuickQuery.TestApp/Program.cs
@@ -6,7 +6,14 @@ namespace QuickQuery.TestApp
     {
         public static void Main(string[] args)
         {
-            var quickQuery = new QuickQuery();
+            var quickQuery = new QuickQuery()
+            {
+                ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\wichu\Documents\Visual Studio\Projects\QuickQuery\QuickQuery.TestApp\Database.mdf"";Integrated Security=True"
+            };
+
+            var items = quickQuery.Query("SELECT Id, Name, Value FROM Items", new { Id = 0, Name = "", Value = 0M });
+
+            items.ForEach(x => Console.WriteLine(x));
 
             Console.WriteLine("Press any key...");
             Console.ReadKey();
